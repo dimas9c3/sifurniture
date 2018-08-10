ffcbcdde150b492b07045e356cd56221c0000b70
dimas_9c3 <dimas9c3@gmail.com>
2018-08-10 21:28:04 +0700

    fix pass db

--- application/views/admin/pengaturan/View_pengaturan.php
+++ application/views/admin/pengaturan/View_pengaturan.php
@@ -1,4 +1,4 @@
-<?php   
+<?php
     $this->load->view('admin/partial/View_header');
     $this->load->view('admin/partial/View_sidebar_left');
 ?>
@@ -18,7 +18,46 @@
 
     <!-- Main content -->
     <section class="content container-fluid">
-       
+		<div class="row">
+            <div class="col-xs-12">
+                <div class="box">
+                    <div class="box">
+                        <div class="box-header">
+                             <a class="btn btn-success btn-flat" href="<?php echo base_url();?>admin/barang/tambah_barang"><span class="fa fa-plus"></span> Tambah Data</a>
+                        <!-- /.box-header -->
+                        </div>
+                        <div class="box-body">
+                            <div class="table-responsive">
+                                <table id="tabel_data" class="table table-striped table-bordered table-hover" style="width: 100%;">
+                                    <thead>
+                                        <tr>
+                                            <th style="height: 60px;">No</th>
+                                            <th>Foto Utama</th>
+                                            <th>ID Barang</th>
+                                            <th>Kategori | Subkategori</th>
+                                            <th>Nama Barang</th>
+                                            <th>Harga Jual</th>
+                                            <th>Diskon</th>
+                                            <th>Harga Akhir</th>
+                                            <th style="min-width: 15%;">Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="content_data">
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.table-responsive -->
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col-xs-12 -->
+        </div>
+        <!-- /.row -->
     </section>
     <!-- /.content -->
 </div>
