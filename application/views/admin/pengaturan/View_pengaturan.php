348fea9579f2c73fc5ea8b5d4e5f13b41a8d0932
dimas_9c3 <dimas9c3@gmail.com>
2018-08-10 21:40:47 +0700

    fix footer

--- application/views/admin/pengaturan/View_pengaturan.php
+++ application/views/admin/pengaturan/View_pengaturan.php
@@ -21,37 +21,14 @@
 		<div class="row">
             <div class="col-xs-12">
                 <div class="box">
-                    <div class="box">
-                        <div class="box-header">
-                             <a class="btn btn-success btn-flat" href="<?php echo base_url();?>admin/barang/tambah_barang"><span class="fa fa-plus"></span> Tambah Data</a>
-                        <!-- /.box-header -->
-                        </div>
-                        <div class="box-body">
-                            <div class="table-responsive">
-                                <table id="tabel_data" class="table table-striped table-bordered table-hover" style="width: 100%;">
-                                    <thead>
-                                        <tr>
-                                            <th style="height: 60px;">No</th>
-                                            <th>Foto Utama</th>
-                                            <th>ID Barang</th>
-                                            <th>Kategori | Subkategori</th>
-                                            <th>Nama Barang</th>
-                                            <th>Harga Jual</th>
-                                            <th>Diskon</th>
-                                            <th>Harga Akhir</th>
-                                            <th style="min-width: 15%;">Aksi</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody id="content_data">
-
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.table-responsive -->
-                        </div>
-                        <!-- /.box-body -->
+                    <div class="box-header">
+                    	<a class="btn btn-success btn-flat" href="<?php echo base_url();?>admin/barang/tambah_barang"><span class="fa fa-plus"></span> Tambah Data</a>
                     </div>
-                    <!-- /.box -->
+					<!-- /.box-header -->
+                    <div class="box-body">
+
+                    </div>
+                    <!-- /.box-body -->
                 </div>
                 <!-- /.box -->
             </div>
--- application/views/frontend/home/View_home.php
+++ application/views/frontend/home/View_home.php
@@ -384,61 +384,6 @@
         </div>
     </div>
 
-    <!-- Scroll Top Button        -->
-    <div id="scrollTopButton"><i class="fa fa-long-arrow-up"></i></div>
-    <footer class="footer bg-black-3">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 brief">
-                    <div class="logo"><img src="<?php echo base_url();?>vendors/frontend/img/logo-light.png" alt="..." width="170"></div>
-                        <p>property murah namun dengan bahan dan kualitas terbaik itulah kami, segera order furniture pada gerai kami di kota anda. dapatkan diskon besar besaran setiap hari.</p>
-                        <ul class="social list-inline">
-                            <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fa fa-pinterest"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
-                        </ul>
-                </div>
-                <div class="col-lg-2 links">
-                    <h3 class="h4 text-thin text-uppercase">Company</h3>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Properties</a></li>
-                        <li><a href="#">Landlords</a></li>
-                        <li><a href="#">Renters</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Pricing</a></li>
-                    </ul>
-                </div>
-          <div class="col-lg-2 links">
-            <h3 class="h4 text-thin text-uppercase">Support</h3>
-            <ul class="list-unstyled">
-              <li><a href="#">Help & FAQ</a></li>
-              <li><a href="#">Policy Privacy</a></li>
-              <li><a href="#">Contact Us</a></li>
-              <li><a href="#">Careers</a></li>
-              <li><a href="#">Our Partners</a></li>
-            </ul>
-          </div>
-          <div class="col-lg-4 newsletter">
-            <h3 class="h4 text-thin text-uppercase">Newsletter</h3>
-            <p>property murah namun dengan bahan dan kualitas terbaik itulah kami, segera order furniture pada gerai kami di kota anda. dapatkan diskon besar besaran setiap hari.</p>
-            <form class="newsletter-form">
-              <div class="form-group">
-                <input type="email" name="email" placeholder="Enter your email address" class="form-control newsletter-form">
-                <button type="submit" class="btn btn-gradient submit"><i class="icon-email-plane"></i></button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      <div class="copyrights bg-black-5">
-        <div class="container text-center">
-          <p>&copy; Copyrights 2018. Template by <a href="https://bootstrapious.com/">dimas9c3@gmail.com</a></p>
-        </div>
-      </div>
-    </footer>
-
     <?php $this->load->view('frontend/partial/view_footer'); ?>
 
 		<script>
